ce214f5b15354db60dab12d93714b4ab1693d449
李翔宇 <7071242@qq.com>
2021-01-25 10:53:58 +0800

    xif

--- src/core/test.js
+++ src/core/test.js
@@ -1,6 +1,7 @@
 import demo from "./demo.json";
 import { ItemSchema } from "./entity/ItemSchema";
-const catId = 50003881;
+const catId = 50003881;  // 度量衡
+// const catId = 122012002; // 单位度量衡
 export default function () {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'http://localhost/item/publish/schema/get?catId=' + catId, true);
